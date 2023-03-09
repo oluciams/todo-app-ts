920dd41940ad00af90406cd386745ff669e43f37
oluciams <oluciams84@gmail.com>
2023-03-09 11:08:35 -0500

    add initial configuration and component

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Todos } from './components/Todos'
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false
+  }
+]
+
+const App: React.FC = () => {
+  const [todos] = useState(mockTodos)
+  return (
+    <Todos todos={todos} />
+  )
+}
+
+export default App
